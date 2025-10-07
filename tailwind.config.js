c44b17a08cef6db4eab4996b838ebde2993c8f43
biskutso <laingolorenz@gmail.com>
2025-10-07 10:44:27 +0800

    Act2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,23 @@ module.exports = {
   content: [
     './templates/**/*.html.twig',
     './assets/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+  safelist: [
+    {
+      pattern: /bg-(red|green|yellow|purple|pink|gray|indigo|emerald)-(100|200|300|400|500|600|700|800|900)/,
+    },
+    {
+      pattern: /text-(red|green|yellow|purple|pink|gray|indigo|emerald)-(100|200|300|400|500|600|700|800|900)/,
+    },
+    {
+      pattern: /hover:bg-(red|green|yellow|purple|pink|gray|indigo|emerald)-(100|200|300|400|500|600|700|800|900)/,
+    }
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
